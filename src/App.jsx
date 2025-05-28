52de1d2002431e7c0f5fd2cf3cddbbe1667c34a6
Javascript_Developer <ah1502651@gmail.com>
2025-05-28 14:42:32 +0500

    Toasting funtionality is also completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import Books from "./pages/Books";
 import Authors from "./pages/Authors";
 import Sales from "./pages/Sales";
 import Reports from "./pages/Reports";
-import Layout from "./components/DashboardContent";  
+import Layout from "./components/DashboardContent";
 import ContentPage from "./pages/ContentPage";
 import ViewAuthors from "./pages/ViewAuthors";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <Router>
+      <ToastContainer
+        position="top-right" 
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+      />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
@@ -19,8 +30,7 @@ function App() {
           <Route path="authors" element={<Authors />} />
           <Route path="sales" element={<Sales />} />
           <Route path="reports" element={<Reports />} />
-          <Route path="/authors/:id" element={<ViewAuthors/>} />
-
+          <Route path="/authors/:id" element={<ViewAuthors />} />
         </Route>
       </Routes>
     </Router>
